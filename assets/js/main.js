f7a65bcccda2ebf5318a9cd983e7a5368b5a8049
posokhin anton <posohin7@gmail.com>
2018-12-02 03:52:00 +0300

    add .gitignore

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,23 @@
 jQuery(".fa-bars").on('click',function($){
     jQuery(".header__menu").slideToggle();
 })
+
+
+jQuery(window).on('scroll', function () {
+    if (jQuery(this).scrollTop() > 200) {
+        jQuery('.btn_up').fadeIn();
+    } else {
+        jQuery('.btn_up').fadeOut();
+    }
+})
+jQuery('.btn_up').on('click', function (e) {
+    e.preventDefault();
+    jQuery('html , body').animate({
+        scrollTop: 0
+    } , 800);
+})
+
+jQuery(window).on('scroll', function(){
+    var ratio = jQuery(document).scrollTop()/((jQuery(document).height()-jQuery(window).height())/100);
+    jQuery('.progressbar').width(ratio+'%');
+})
